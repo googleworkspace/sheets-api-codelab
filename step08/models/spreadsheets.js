577753c290c3229e4d60efc68d57463b9429c9a6
Eric Koleda <ekoleda@google.com>
2017-03-29 09:48:35 -0400

    Remove trailing commas in later steps.

--- step08/models/spreadsheets.js
+++ step08/models/spreadsheets.js
@@ -23,7 +23,7 @@ module.exports = function(sequelize, DataTypes) {
   var Spreadsheet = sequelize.define('Spreadsheet', {
     id: {type: DataTypes.STRING, allowNull: false, primaryKey: true},
     sheetId: {type: DataTypes.INTEGER, allowNull: false},
-    name: {type: DataTypes.STRING, allowNull: false},
+    name: {type: DataTypes.STRING, allowNull: false}
   });
 
   return Spreadsheet;
